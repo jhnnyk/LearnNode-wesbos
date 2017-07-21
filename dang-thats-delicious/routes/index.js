099543b8fdae57f52917acf65c309169cd5741cf
John Knotts <jhnnyk@gmail.com>
2017-07-21 15:11:05 -0600

    core concept - routing

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -3,7 +3,16 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  const john = { name: 'John', age: 41, cool: true }
+  // res.send('Hey! It works!');
+  // res.json(john)
+  // res.send(req.query.name)
+  res.json(req.query)
 });
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('')
+  res.send(reverse)
+})
 
 module.exports = router;
