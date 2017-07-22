b9a6383592cbd7c4d59a4d4eb599a2a2d4c83abe
John Knotts <jhnnyk@gmail.com>
2017-07-22 16:38:06 -0600

    core concept - controllers and the MVC pattern

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -1,22 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController')
 
-// Do work here
-router.get('/', (req, res) => {
-  const john = { name: 'John', age: 41, cool: true }
-  // res.send('Hey! It works!');
-  // res.json(john)
-  // res.send(req.query.name)
-  // res.json(req.query)
-  res.render('hello', {
-    name: 'John',
-    dog: req.query.dog
-  })
-});
-
-router.get('/reverse/:name', (req, res) => {
-  const reverse = [...req.params.name].reverse().join('')
-  res.send(reverse)
-})
+router.get('/', storeController.homePage )
 
 module.exports = router;
