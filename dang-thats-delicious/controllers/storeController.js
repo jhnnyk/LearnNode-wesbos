9187963b65e9c032bb2071462c80550bdfa98231
John Knotts <jhnnyk@gmail.com>
2017-08-04 16:30:05 -0600

    creating an editing flow for stores

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -29,3 +29,14 @@ exports.editStore = async (req, res) => {
   // 3. render out the edit form so the user can update their store
   res.render('editStore', { title: `Edit ${store.name}`, store: store })
 }
+
+exports.updateStore = async (req, res) => {
+  // find and update the store
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true, // return the new store instead of the old one
+    runValidators: true
+  }).exec()
+  // redirect them to the store and tell them it worked
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store</a>`)
+  res.redirect(`/stores/${store._id}/edit`)
+}
